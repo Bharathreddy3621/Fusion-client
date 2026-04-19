2fab4dcec2f230578c41a88e6b3ebf42d6194100
Bharathreddy3621 <bharathmadatala2@gmail.com>
2026-04-19 13:51:39 +0530

    Remove gymkhanaPreviewMain.jsx - not needed

--- src/gymkhanaPreviewMain.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import GymkhanaDashboard from './Modules/Gymkhana/GymkhanaDashboard';
-
-const GymkhanaPreviewMain = () => {
-    return <GymkhanaDashboard />;
-};
-
-export default GymkhanaPreviewMain;
