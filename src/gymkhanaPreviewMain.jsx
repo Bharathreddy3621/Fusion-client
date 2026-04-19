8f99218c12b1c7753f51debc4eef7514a4f576d5
Bharath Reddy <bharathmadatala2@gmail.com>
2026-04-19 13:41:46 +0530

    Create src/gymkhanaPreviewMain.jsx to import and render GymkhanaDashboard

---
+++ src/gymkhanaPreviewMain.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import GymkhanaDashboard from './GymkhanaDashboard';
+
+const GymkhanaPreviewMain = () => {
+    return <GymkhanaDashboard />;
+};
+
+export default GymkhanaPreviewMain;
